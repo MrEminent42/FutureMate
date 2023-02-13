2d09e3979ff3c9349249a9273d54c17ae9c2975c
MrEminent42 <github@leohorwitz.com>
2023-02-13 13:21:51 -0800

    persist filters across refresh

--- src/jotai/filtersAtom.ts
+++ src/jotai/filtersAtom.ts
@@ -1,16 +1,17 @@
 import { atom } from "jotai";
 import { CleanlinessResponse, LocationResponse, LoudnessResponse, StartDate } from "../types/MatchingQuestions";
+import { atomWithStorage } from 'jotai/utils';
 
 const includeNoResponseFiltersAtom = atom(true);
 
-const locationFilterAtom = atom<LocationResponse | null>(null)
+const locationFilterAtom = atomWithStorage<LocationResponse | null>('locationFilter', null)
 
-const startDateFilterAtom = atom<StartDate[]>([])
-const cleanlinessFilterAtom = atom<CleanlinessResponse[]>([])
-const loudnessFiltersAtom = atom<LoudnessResponse[]>([])
+const startDateFilterAtom = atomWithStorage<StartDate[]>('startDateFilter', [])
+const cleanlinessFilterAtom = atomWithStorage<CleanlinessResponse[]>('cleanlinessFilter', [])
+const loudnessFiltersAtom = atomWithStorage<LoudnessResponse[]>('loudnessFilter', [])
 
-const householdSizeFilterAtom = atom<number[]>([])
-const budgetFilterAtom = atom<number | null>(null)
+const householdSizeFilterAtom = atomWithStorage<number[]>('householdSizeFilter', [])
+const budgetFilterAtom = atomWithStorage<number | null>('budgetFilter', null)
 
 export {
     locationFilterAtom,
